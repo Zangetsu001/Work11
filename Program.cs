b1dd0c11522a6c27756d549a8dd8e577d28930c8
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-12-10 14:37:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        /// <summary>
+        ///  найти сумму всех чисел
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] omas= { 5, 7, 1, 9, 5, 3, 6, 5, 4 };
+            int sum = omas.Sum();
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
