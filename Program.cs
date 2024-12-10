ba673eb8a704f9e72b02094fda0051b717fb1e78
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-12-10 13:41:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Отсортировать одномерный массив по возростанию и по убыванию
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            Array.Sort(people);
+            foreach(var el in people)
+            {
+                Console.WriteLine(el);
+            }
+            Console.WriteLine("");
+            Array.Reverse(people);
+            foreach(var el in people)
+            {
+                Console.WriteLine(el);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
