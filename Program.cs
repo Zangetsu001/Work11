89be2b2854314fac981bfc92251c489b1915038c
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-12-10 14:28:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        /// <summary>
+        /// Удалить всек четные элементы из одномерного массива
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] omas = { 5, 7, 1, 9, 5, 3, 6, 5, 4 };
+            omas = omas.Where(x => x % 2 != 0).ToArray();
+            foreach (var el in omas)
+            {
+                Console.Write($"{el}\t");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
